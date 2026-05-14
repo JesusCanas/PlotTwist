7f65d92d3bd00ee5693462c6242236bef7e50194
JesusCanas <jesuscanas2007@gmail.com>
2026-05-14 09:55:21 +0200

    hecho ver todas las peliculas

--- front/js/main.js
+++ front/js/main.js
@@ -14,19 +14,4 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    const divPelicula = document.getElementById("pelis");
-    
-    fetch(urlPeliculas)
-        .then(response => response.json())
-        .then(peliculas => {
-            console.log(peliculas);
-            peliculas.forEach(element => {
-                const divPosterPelis = document.createElement('div');
-                divPosterPelis.classList.add("poster");
-                const imgPeliculas = document.createElement('img');
-                imgPeliculas.src = element.imagen;
-                divPosterPelis.appendChild(imgPeliculas);
-                divPelicula.appendChild(divPosterPelis);
-            });
-        });
 });
--- front/js/peliculas.js
+++ front/js/peliculas.js
@@ -1,7 +1,18 @@
-const urlPeliculasTodos="http://98.84.88.91:8082//obtenerTipo//";
-const urlPeliculasTodos="http://98.84.88.91:8082/peliculas//mostrarTodo";
-const urlPeliculasTodos="http://98.84.88.91:8082/peliculas//mostrarTodo";
-const urlPeliculasTodos="http://98.84.88.91:8082/peliculas//mostrarTodo";
+const urlPeliculas = 'http://98.84.88.91:8082/metrajes/obtenerTipo?tipoMetraje=PELICULA';
 document.addEventListener("DOMContentLoaded",()=>{
-
+    const divPelicula = document.getElementById("pelis");
+    
+    fetch(urlPeliculas)
+        .then(response => response.json())
+        .then(peliculas => {
+            console.log(peliculas);
+            peliculas.forEach(element => {
+                const divPosterPelis = document.createElement('div');
+                divPosterPelis.classList.add("poster");
+                const imgPeliculas = document.createElement('img');
+                imgPeliculas.src = element.imagen;
+                divPosterPelis.appendChild(imgPeliculas);
+                divPelicula.appendChild(divPosterPelis);
+            });
+        });
 });
