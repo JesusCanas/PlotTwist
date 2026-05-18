2ccf2e90223e7bc68b6c1398185871e3df0d22e8
JesusCanas <jesuscanas2007@gmail.com>
2026-05-18 11:07:09 +0200

    realizado buscador y filtros

--- front/js/metraje.js
+++ front/js/metraje.js
@@ -2,16 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const selectValoracion = document.getElementById("valoracion");
     const selectGenero = document.getElementById("genero");
     const selectAnyo = document.getElementById("anyo");
+    const buscador = document.getElementById("buscador");
+
+    const BASE_PELIS = 'http://localhost:8082/metrajes/obtenerFiltrados?tipoMetraje=PELICULA';
+    const BASE_SERIES = 'http://localhost:8082/metrajes/obtenerFiltrados?tipoMetraje=SERIE';
 
     const divPelicula = document.getElementById("pelis");
     const divSerie = document.getElementById("seri");
+    let temporizador;
 
     function crearTarjeta(element, tipo) {
         const divPoster = document.createElement('div');
         divPoster.classList.add("poster");
 
         const img = document.createElement('img');
-        img.src = element.imagen;
+        img.src = element.imagenURL;
         img.alt = element.titulo;
 
         const titulo = document.createElement('p');
@@ -23,54 +28,73 @@ document.addEventListener("DOMContentLoaded", () => {
         return divPoster;
     }
 
-    function filtrar() {
+    async function filtrar() {
+        const buscadorVal = buscador.value;
         const valoracionVal = selectValoracion.value;
         const generoVal = selectGenero.value;
         const anyoVal = selectAnyo.value;
 
-        let urlPeliculas = 'http://98.84.88.91:8082/metrajes/obtenerFiltrados?tipoMetraje=PELICULA';
-        let urlSeries = 'http://98.84.88.91:8082/metrajes/obtenerFiltrados?tipoMetraje=SERIE';
+        let urlPelis = BASE_PELIS;
+        let urlSeries = BASE_SERIES;
 
+        if (buscadorVal) {
+            urlPelis += `&nombre=${buscadorVal}`;
+            urlSeries += `&nombre=${buscadorVal}`;
+        }
         if (valoracionVal) {
-            urlPeliculas += `&valoracion=${valoracionVal}`;
+            urlPelis += `&valoracion=${valoracionVal}`;
             urlSeries += `&valoracion=${valoracionVal}`;
         }
         if (generoVal) {
-            urlPeliculas += `&generos=${generoVal}`;
+            urlPelis += `&generos=${generoVal}`;
             urlSeries += `&generos=${generoVal}`;
         }
         if (anyoVal) {
-            urlPeliculas += `&anyo=${anyoVal}`;
+            urlPelis += `&anyo=${anyoVal}`;
             urlSeries += `&anyo=${anyoVal}`;
         }
 
-        console.log(urlPeliculas);
-        console.log(urlSeries);
+        if (divPelicula) {
+            while (divPelicula.firstChild) {
+                divPelicula.removeChild(divPelicula.firstChild);
+            }
+            const resPelis = await fetch(urlPelis);
+            const dataPelis = await resPelis.json();
+            dataPelis.forEach(item => divPelicula.appendChild(crearTarjeta(item, 'peliculas')));
+        }
+
+        if (divSerie) {
+            while (divSerie.firstChild) {
+                divSerie.removeChild(divSerie.firstChild);
+            }
+            const resSeries = await fetch(urlSeries);
+            const dataSeries = await resSeries.json();
+            dataSeries.forEach(item => divSerie.appendChild(crearTarjeta(item, 'series')));
+        }
+    }
+
+    function debounce(funcion, tiempo) {
+        clearTimeout(temporizador);
+        temporizador = setTimeout(funcion, tiempo);
     }
 
     selectValoracion.addEventListener("change", filtrar);
-    selectGenero.addEventListener("change", filtrar);
+    selectGenero.addEventListener("change", () => debounce(filtrar, 2000));
     selectAnyo.addEventListener("change", filtrar);
+    buscador.addEventListener("input", () => debounce(filtrar, 2000));
 
-    if (divPelicula) {
-        fetch('http://98.84.88.91:8082/metrajes/obtenerTipo?tipoMetraje=PELICULA')
-            .then(res => res.json())
-            .then(data => {
-                data.forEach((item) => {
-                    divPelicula.appendChild(crearTarjeta(item, 'peliculas'));
-                });
-            })
-            .catch(error => console.error("Error cargando películas:", error));
+    async function cargarInicial() {
+        if (divPelicula) {
+            const res = await fetch(BASE_PELIS);
+            const data = await res.json();
+            data.forEach(item => divPelicula.appendChild(crearTarjeta(item, 'peliculas')));
+        }
+        if (divSerie) {
+            const res = await fetch(BASE_SERIES);
+            const data = await res.json();
+            data.forEach(item => divSerie.appendChild(crearTarjeta(item, 'series')));
+        }
     }
 
-    if (divSerie) {
-        fetch('http://98.84.88.91:8082/metrajes/obtenerTipo?tipoMetraje=SERIE')
-            .then(res => res.json())
-            .then(data => {
-                data.forEach((item) => {
-                    divSerie.appendChild(crearTarjeta(item, 'series'));
-                });
-            })
-            .catch(error => console.error("Error cargando series:", error));
-    }
+    cargarInicial();
 });
