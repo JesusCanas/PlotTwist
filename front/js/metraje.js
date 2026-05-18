405dcb49eff5524acbfa11daceba27b4e6af5914
JesusCanas <jesuscanas2007@gmail.com>
2026-05-18 09:28:37 +0200

    cambios

--- front/js/metraje.js
+++ front/js/metraje.js
@@ -1,23 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const selectValoracion = document.getElementById("valoracion");
+    const selectGenero = document.getElementById("genero");
+    const selectAnyo = document.getElementById("anyo");
 
     const divPelicula = document.getElementById("pelis");
     const divSerie = document.getElementById("seri");
-    const filtros = document.getElementsByClassName("filtros")
-    const valoracion = document.getElementById("valoracion");
-    const anyo = document.getElementById("anyo");
-    const genero = document.getElementById("genero");
 
-    function crearTarjeta(element, index, tipo) {
+    function crearTarjeta(element, tipo) {
         const divPoster = document.createElement('div');
         divPoster.classList.add("poster");
 
         const img = document.createElement('img');
-        const numeroCarpeta = index + 1;
-        
-        img.src = `https://proyecto-plottwist.s3.us-east-1.amazonaws.com/metrajes/${tipo}/${numeroCarpeta}/poster.jpg`;
-        img.alt = element.titulo; 
+        img.src = element.imagen;
+        img.alt = element.titulo;
 
-        const titulo = document.createElement('p'); 
+        const titulo = document.createElement('p');
         titulo.textContent = element.titulo;
         titulo.classList.add("titulo-texto");
 
@@ -25,19 +22,55 @@ document.addEventListener("DOMContentLoaded", () => {
         divPoster.appendChild(titulo);
         return divPoster;
     }
+
+    function filtrar() {
+        const valoracionVal = selectValoracion.value;
+        const generoVal = selectGenero.value;
+        const anyoVal = selectAnyo.value;
+
+        let urlPeliculas = 'http://98.84.88.91:8082/metrajes/obtenerFiltrados?tipoMetraje=PELICULA';
+        let urlSeries = 'http://98.84.88.91:8082/metrajes/obtenerFiltrados?tipoMetraje=SERIE';
+
+        if (valoracionVal) {
+            urlPeliculas += `&valoracion=${valoracionVal}`;
+            urlSeries += `&valoracion=${valoracionVal}`;
+        }
+        if (generoVal) {
+            urlPeliculas += `&generos=${generoVal}`;
+            urlSeries += `&generos=${generoVal}`;
+        }
+        if (anyoVal) {
+            urlPeliculas += `&anyo=${anyoVal}`;
+            urlSeries += `&anyo=${anyoVal}`;
+        }
+
+        console.log(urlPeliculas);
+        console.log(urlSeries);
+    }
+
+    selectValoracion.addEventListener("change", filtrar);
+    selectGenero.addEventListener("change", filtrar);
+    selectAnyo.addEventListener("change", filtrar);
+
     if (divPelicula) {
         fetch('http://98.84.88.91:8082/metrajes/obtenerTipo?tipoMetraje=PELICULA')
             .then(res => res.json())
             .then(data => {
-                data.forEach((item, i) => divPelicula.appendChild(crearTarjeta(item, i, 'peliculas')));
-            });
-        fetch('http://98.84.88.91:8082/metrajes/obtenerTipo?tipoMetraje=PELICULA')
+                data.forEach((item) => {
+                    divPelicula.appendChild(crearTarjeta(item, 'peliculas'));
+                });
+            })
+            .catch(error => console.error("Error cargando películas:", error));
     }
+
     if (divSerie) {
         fetch('http://98.84.88.91:8082/metrajes/obtenerTipo?tipoMetraje=SERIE')
             .then(res => res.json())
             .then(data => {
-                data.forEach((item, i) => divSerie.appendChild(crearTarjeta(item, i, 'series')));
-            });
+                data.forEach((item) => {
+                    divSerie.appendChild(crearTarjeta(item, 'series'));
+                });
+            })
+            .catch(error => console.error("Error cargando series:", error));
     }
 });
