b6a17bfdab8d7a7928e50cb263f3a536b4b8626f
JesusCanas <jesuscanas2007@gmail.com>
2026-05-21 10:47:00 +0200

    realizado personas.js

--- front/js/personas.js
+++ front/js/personas.js
@@ -1,5 +1,62 @@
 document.addEventListener("DOMContentLoaded", () => {
     const parametros = new URLSearchParams(window.location.search);
-    const id = parametros.get("id");
-    const URL_PERSONA = `http://localhost:8082/personas/mostrarDestacados?cantidad=3&idPersona=id{}`
+    const idPersona = parametros.get("idPersona");
+    const URL_PERSONA = `http://98.84.88.91:8082/personas/mostrarDestacados?cantidad=3&idPersona=${idPersona}`;
+
+    function crearPersonaFoto(elemento){
+        const seccion_personaFoto = document.querySelector(".persona-foto");
+        const imagen = document.createElement("img");
+        const lista = document.createElement("ul");
+        const nombrePersona = document.createElement("h1");
+        const puntoFecha = document.createElement("li");
+        const puntoNacionalidad = document.createElement("li");
+        imagen.src = elemento.imagenURL;
+        imagen.alt= elemento.nombre +" " +element.apellido;
+        nombrePersona.textContent=elemento.nombre + ' ' + elemento.apellido;
+        puntoFecha.innerHTML='<strong>Fecha de nacimiento:</strong>' + elemento.fechaDeNacimiento;
+        puntoNacionalidad.innerHTML = '<strong>Nacionalidad:</strong>' + elemento.nacionalidad;
+        seccion_personaFoto.appendChild(imagen);
+        seccion_personaFoto.appendChild(nombrePersona);
+        seccion_personaFoto.appendChild(lista);
+        lista.appendChild(puntoFecha);
+        lista.appendChild(puntoNacionalidad);
+    }
+    function crearPersonaBiografia(elemento){
+        const seccion_biografia = document.querySelector(".persona-biografia");
+        const parrafo_biografia = document.createElement("p");  
+    }
+    function crearPoster(elemento) {
+        const seccion_poster = document.querySelector(".persona-metrajes");
+        const li = document.createElement("li");
+        const img = document.createElement("img");
+        const span = document.createElement("span");
+        li.className = "metraje-card";
+        img.src = metraje.imagenURL;
+        img.alt = metraje.titulo;
+        span.textContent = metraje.titulo;
+        li.addEventListener("click", () => {
+            window.location.href = `detalle-metraje.html?id=${metraje.id}`;
+        });
+        const peliculas = data.filter(item => !item.numTemporadas);
+        const series = data.filter(item => item.numTemporadas);
+        if(peliculas){
+            tipoMetraje.textContent="PELICULAS";
+        }
+        else {
+            tipoMetraje.textContent="SERIES";
+        }
+        seccion_poster.appendChild(tipoMetraje);
+        li.appendChild(img);
+        li.appendChild(span);
+        seccion_poster.appendChild(li);
+    }
+
+    function crearPersona(elemento){
+        crearPersonaFoto(elemento);
+        crearPersonaBiografia(elemento);
+        crearPoster(elemento);
+    }
+    fetch(URL_PERSONA)
+    .then (res => res.json())
+    .then (data => crearPersona(data));
 });
--- src/main/java/org/paloma/plottwist/controller/MetrajesController.java
+++ src/main/java/org/paloma/plottwist/controller/MetrajesController.java
@@ -6,7 +6,6 @@ import org.paloma.plottwist.model.Metraje;
 import org.paloma.plottwist.model.TipoMetraje;
 import org.paloma.plottwist.service.MetrajeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
